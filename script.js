09dbdfe4d3bd3995d13a322184c90435716b98c0
arne <arne.vanheule@ugent.be>
2022-11-28 16:57:36 +0100

    added functionality

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-$(document).ready(function() {
-    $(".ace of spades card").click(function(){
-        alert('Clicked !!');
-    });
-});
+const rules = {
+    "spades_13" : "Zwarte koning niks speciaal"
+}
+
+
+
+window.onload=function(){
+
+    const cardnumbers = ["1", "13", "12", "11", "10"]
+    for (const number of cardnumbers){
+        const cardvalue = "spades_" + number
+
+        const button = document.getElementById(cardvalue);
+        button.myParam = cardvalue;
+        button.addEventListener("click", getCardInfo);
+    }
+};
+
+
+function getCardInfo(evt) {
+    const cardvalue = evt.currentTarget.myParam
+
+    if (cardvalue in rules){
+        alert(rules[cardvalue])
+    }
+
+}
